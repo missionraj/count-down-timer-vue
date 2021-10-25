f0b39f1976e6b452dec490d586bd8b466886906e
raj rana <raj.rana507@gmail.com>
2021-10-25 21:55:52 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+</script>
+
+<template>
+  <div class="container">
+
+  </div>
+</template>
+
+<style>
+  .container { 
+    height: 100vh;
+    background-image: url('@/assets/mountain-rocks.jpg');
+    background-attachment: fixed;
+    background-size: auto 100%;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+</style>
